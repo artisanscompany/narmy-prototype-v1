068fa61e27b72b30a567445d228fbd18271e0086
Usman <usman@cr4fts.com>
2026-04-01 02:30:45 +0300

    feat: add maskSensitive utility for profile field masking

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function maskSensitive(value: string): string {
+  if (value.length <= 3) return '****'
+  return '****' + value.slice(-3)
+}
